ada9129da27478b1a7c43e64997ec7aa2e911853
Amine_88 <amin.ouni@neopolis-dev.com>
2023-12-14 10:05:31 +0100

    Refactor code for improved performance and readability

---
+++ src/layouts/RootFooter.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const RootFooter: React.FC = (props: Props) => {
+  return <div>RootFooter</div>;
+};
+
+export default RootFooter;
---
+++ src/layouts/RootHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const RootHeader: React.FC = (props: Props) => {
+  return <div>RootHeader</div>;
+};
+
+export default RootHeader;
